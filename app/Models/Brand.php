644080927145feb8e180315cd9b0a9afca24dd28
jeishanul <shishir.picotech@gmail.com>
2023-08-23 15:48:26 +0600

    add brand

---
+++ app/Models/Brand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Status;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Brand extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $dates = ['deleted_at'];
+
+    protected $casts = [
+        'status' => Status::class,
+    ];
+
+    public function media()
+    {
+    	return $this->belongsTo(Media::class,'media_id');
+    }
+}
---
+++ database/migrations/2023_08_23_063211_create_brands_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Media;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained((new User())->getTable());
+            $table->string('name');
+            $table->foreignId('media_id')->nullable()->constrained((new Media())->getTable());
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
